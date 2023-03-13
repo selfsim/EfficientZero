d201cf65384c0672870365218eb276a2fe2ac948
Selfsim <selfsim@protonmail.com>
2023-03-12 23:46:46 -0700

    experiment parameters changed to adhere to node restrictions

--- experiment.sh
+++ experiment.sh
@@ -1,20 +1,18 @@
 set -ex
-#export CUDA_DEVICE_ORDER='PCI_BUS_ID'
-#export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 python3 main.py \
   --env alien \
   --case atari \
   --opr train \
   --force \
-  --num_gpus 8 \
-  --num_cpus 32 \
-  --cpu_actor 14 \
-  --gpu_actor 20 \
+  --num_gpus 4 \
+  --num_cpus 16 \
+  --cpu_actor 16 \
+  --gpu_actor 8 \
   --seed 0 \
   --p_mcts_num 4 \
   --use_priority \
   --use_max_priority \
   --amp_type 'torch_amp' \
-  --info 'E0 - Alien_test' \
+  --info 'E0 - Alien_test' \ 
   --save_video
